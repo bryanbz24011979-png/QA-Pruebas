d522ac9411f9ee1c339bd90d011ff234ebfd2eca
Bryan Bolanos <Chronos@ZEUS>
2025-12-15 17:56:18 -0600

    Version 004 con triggers

--- Saludo.sh
+++ Saludo.sh
@@ -1,3 +1,4 @@
 echo "Hola Mundo desde Jenkins :) - Desde Aplicativo"
 echo "Probando Trigger desde Jenkins y Github"
 echo "Probando Trigger con Webhook desde git y usando grook"
+echo "Prueba Final GIT"
