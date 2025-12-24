097044a3486bd84265dadfa769793bb44c96df21
Bryan Bolaños <bryanbz24011979@gmail.com>
2025-12-24 17:14:02 -0600

    Actualizacion del ambiente de PROD

--- rama_develop.py
+++ rama_develop.py
@@ -1,3 +1,3 @@
 print("Este mensaje le pertenece a la rama develop")
 print("Segundo cambio en la rama develop")
-print("Cambio para subir como desarrollo final a GitHub....DESARROLLO A PRODUCCION 2")
+print("Cambio para subir como desarrollo final a GitHub... Act ambiente de Prod")
